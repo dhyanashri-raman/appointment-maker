93d0a0d2b69f3716f94bbae723484b418e70f86c
sk2539 <sk2539@scarletmail.rutgers.edu>
2024-09-23 18:11:16 -0400

    commit again

--- clinic/Appointment.java
+++ clinic/Appointment.java
@@ -1,4 +1,42 @@
 package clinic;
 
-public class Appointment {
+public class Appointment implements Comparable <Appointment> {
+    private Date date;
+    private Timeslot timeslot; // will the timeslot be in military time because there is no mention of AM or PM?
+    private Profile patient;
+    private Provider provider;
+
+    @Override
+    public int compareTo(Appointment o) { // which instance variable should we compare for this method?
+        int dateComparison = this.date.compareTo(o.date);
+
+        // if the timeslot and date and both equal, that means they are the same appointment!
+        if (dateComparison == 0) {
+            int timeslotComparison = this.timeslot.compareTo(o.timeslot); // why is that warning there?
+            return timeslotComparison;
+        }
+
+        return dateComparison; // if the timeslots are not the same, it'll return int difference in dates
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o instanceof Appointment) { // checks to make sure the object o is of type Appointment
+            Appointment appt = (Appointment) o;
+
+            // making sure that each instance variable is not null and has the same value as the comparing Appointment object
+            return (this.date != null ? this.date.equals(appt.date) : appt.date == null)
+                    && (this.timeslot != null ? this.timeslot.equals(appt.timeslot) : appt.timeslot == null)
+                    && (this.patient != null ? this.patient.equals(appt.patient) : appt.patient == null)
+                    && (this.provider != null ? this.provider.equals(appt.provider) : appt.provider == null);
+
+        }
+        return false;
+    }
+
+    @Override
+    public String toString() {
+        return this.date + " " + this.timeslot + " " + this.patient + " " + this.provider;
+    }
+
 }
--- clinic/Date.java
+++ clinic/Date.java
@@ -1,4 +1,140 @@
 package clinic;
 
-public class Date {
+import java.util.Calendar;
+
+public class Date implements Comparable<Date> {
+    private int year;
+    private int month;
+    private int day;
+
+    public static final int QUADRENNIAL = 4;
+    public static final int CENTENNIAL = 100;
+    public static final int QUATERCENTENNIAL = 400;
+
+    //check if the date is a valid calendar date
+    // THIS METHOD IS STILL IN PROGRESS
+    public boolean isValid() {
+        // Calendar object with current date and time
+        Calendar calendar = Calendar.getInstance();
+        calendar.setLenient(false);
+
+        int yearToday = calendar.get(Calendar.YEAR);
+        int monthToday = calendar.get(Calendar.MONTH) + 1; // Month is zero-based so add 1
+        int dayToday = calendar.get(Calendar.DAY_OF_MONTH);
+
+        // checking if it is a valid calendar date
+        if (this.month<1 || this.month>12) {
+            return false;
+        }
+
+        // add return statement checking all helper method calls
+
+    }
+
+
+    // HELPER METHOD 1: checking if the year is a leap year
+    public boolean isLeapYear() {
+        boolean leapYearCheck;
+        if (this.year%QUADRENNIAL==0) {
+            return false;
+        }
+        else if (this.year%CENTENNIAL==0) {
+            if (this.year%QUATERCENTENNIAL==0) {
+                return false;
+            }
+            else {
+                return true;
+            }
+        }
+        else {
+            return true;
+        }
+    }
+
+    // HELPER METHOD 2: checking if the date is today's date
+    public boolean isToday() {
+        Calendar calendar = Calendar.getInstance();
+        calendar.setLenient(false);
+        return (this.year==calendar.get(Calendar.YEAR)
+                && this.month==calendar.get(Calendar.MONTH)
+                && this.day==calendar.get(Calendar.DAY_OF_MONTH));
+    }
+
+    // HELPER METHOD 3: checking to see if the date is before today
+    public boolean isBeforeToday() {
+
+        if (this.year<Calendar.YEAR) {
+            return false;
+        }
+        else if (this.month<Calendar.MONTH) {
+            return false;
+        }
+        else if (this.day<Calendar.DAY_OF_MONTH){
+            return false;
+        }
+        // if all these if statements are false that means the date is after the current date so we return true
+        return true;
+    }
+
+    // HELPER METHOD 4: checking if the date is on a saturday or sunday
+    public boolean onWeekend() {
+        // creating an appointment date with this object's year, month and day
+        Calendar appointmentDate = new Calendar.Builder()
+                .setDate(this.year, this.month - 1, this.day)
+                .build();
+        int dayofweek = appointmentDate.get(Calendar.DAY_OF_WEEK);
+        return (dayofweek== Calendar.SATURDAY || dayofweek == Calendar.SUNDAY);
+    }
+
+    // HELPER METHOD 5: checking to make sure the date is within 6 months from current date
+    public boolean isWithinSixMonths() {
+        // creating an appointment date with this object's year, month, and day
+        Calendar appointmentDate = new Calendar.Builder()
+           .setDate(this.year, this.month - 1+6, this.day)
+                    .build();
+        // creating an appointment date 6 months from now
+        Calendar sixMonthsLater = new Calendar.Builder()
+                .setDate(this.year, this.month - 1+6, this.day)
+                .build();
+        return appointmentDate.before(sixMonthsLater);
+    }
+
+    // REMEMBER TO INCLUDE TESTBED MAIN HERE!
+
+    @Override
+    public int compareTo(Date o) { // in which unit should this return
+        int yearComparison = this.year - o.year;
+        int monthComparison = this.month - o.month;
+        int dayComparison = this.day - o.day;
+
+        if (yearComparison!=0) {
+            return yearComparison;
+        }
+
+        if (monthComparison!=0) {
+            return monthComparison;
+        }
+
+        // if the year and month are both equal, then we return the difference in the days
+        return dayComparison;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o instanceof Date) { // checks to make sure the object o is of type Date
+            Date dateEx = (Date) o;
+
+            // making sure that each instance variable is not null and has the same value as the comparing Date object
+            return this.year==dateEx.year
+                    && this.month==dateEx.month
+                    && this.day==dateEx.day;
+        }
+
+        return false;
+    }
+
+    @Override
+    public String toString() { // not sure if this is correct
+        return this.month + "/" + this.day + "/" + this.year; // MM/DD/YY format
+    }
 }
